cee3c9ef7e5abbdf2559638090d37f5eecd879ce
mateonoel2 <mateo.noel@utec.edu.pe>
2023-06-09 12:51:09 -0500

    me funciona

--- main.cpp
+++ main.cpp
@@ -1,84 +1,138 @@
-#include <bits/stdc++.h>
+#ifndef MLP_H
+#define MLP_H
+
+#include <boost/container/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/operation.hpp>
+#include <iostream>
+#include <fstream>
+
+typedef boost::numeric::ublas::matrix<double> Mdouble;
+typedef boost::container::vector<int> vec;
+
 using namespace std;
 
 class ActivationFunction {
-    private:
-        double value;
-    public:
-        double sigmoid(double value) {
-            return 1 / (1 + exp(-value));
-        }
+private:
+    double value;
+public:
+    double sigmoid(double value) {
+        return 1 / (1 + exp(-value));
+    }
 
-        double relu(double value) {
-            return max(0.0, value);
-        }
+    double relu(double value) {
+        return max(0.0, value);
+    }
 
-        double tanh(double value) {
-            return (exp(value) - exp(-value)) / (exp(value) + exp(-value));
-        }
+    double tanh(double value) {
+        return (exp(value) - exp(-value)) / (exp(value) + exp(-value));
+    }
 
-        double apply(string activationFunction, double value) {
-            if (activationFunction == "sigmoid") {
-                return sigmoid(value);
-            } else if (activationFunction == "relu") {
-                return relu(value);
-            } else if (activationFunction == "tanh") {
-                return tanh(value);
-            } else {
-                return value;
-            }
+    double apply(string activationFunction, double value) {
+        if (activationFunction == "sigmoid") {
+            return sigmoid(value);
+        } else if (activationFunction == "relu") {
+            return relu(value);
+        } else if (activationFunction == "tanh") {
+            return tanh(value);
+        } else {
+            return value;
         }
+    }
 };
 
-// class Neuron {
-//     private:
-//         double value;
-//         double bias;
-//         double weight;
-//         ActivationFunction activationFunction;
-//     public:
-//         Neuron(double value, double bias, double weight, ActivationFunction activationFunction) {
-//             this->value = value;
-//             this->bias = bias;
-//             this->weight = weight;
-//             this->activationFunction = activationFunction;
-//         }
-// };
+class MLP {
+private:
 
-// class Layer {
-//     private:
-//         Neuron neurons[];
-//     public:
-//         Layer(Neuron neurons[]) {
-//             this->neurons = neurons;
-//         }
-// };
+    boost::container::vector<ActivationFunction*> f; //Neuronas por capa
 
-// class MLP {
-//     private:
-//         Layer layers[];
-//     public:
-//         MLP(Layer layers[]) {
-//             this->layers = layers;
-//         }
-// };
+    vec pPerLayer; //Numero de perceptrones por capa (hidden layer)
 
-int main() {
-    cout << "Hello World!" << endl;
+    boost::container::vector<Mdouble> pesos; //Pesos
 
-    // Initalize MLP with data
+    boost::container::vector<Mdouble> outputs; //Outputs antes de la funcion de activacion
 
-    // Initialize layer and neurons
+    int n_features,n_outputs,n_capas; // num capas ocultas
 
-    // Initialize weights
+    boost::container::vector <boost::container::vector<double>> feature_vectors; //Vectores Caracteristicas
+    boost::container::vector <bool> Y;
 
-    // Initialize bias
+    boost::container::vector <boost::container::vector<double>> X_train, X_test;
+    boost::container::vector<bool> Y_train, Y_test;
 
-    // Initialize activation function
+    Mdouble input;
+    Mdouble MLP_ouput;
 
-    ActivationFunction activationFunction;
-    double value = activationFunction.apply("sigmoid", 0.5);
-    // Print value double
-    cout << value << endl;
+public:
+    MLP(vec initial_states,int n_out,boost::container::vector<ActivationFunction*> actfunct) {
+        pPerLayer = initial_states;
+        n_capas = pPerLayer.size();
+        n_outputs = n_out;
+        f = actfunct;
+        load_data();
+        n_features = feature_vectors[0].size();
+
+        initialization();
+        shuffleAndSplit();
+    }
+
+    void load_data(){
+        ifstream archivo("train.csv");
+        if (archivo.is_open()) {
+            string campos[128], fila;
+            int numeroFilas = 0;
+            while (!archivo.eof()) {
+                getline(archivo, fila);
+                istringstream stringStream(fila);
+                unsigned int contador = 0;
+                while (getline(stringStream, fila, ',')) {
+                    campos[contador] = fila;
+                    contador++;
+                }
+                boost::container::vector <double> filaData;
+                for (int i = 0; i < 128; i++) {
+                    if (i != 1) {
+                        filaData.emplace_back(stof(campos[i]));
+                    } else {
+                        if (campos[i] == "M") {
+                            Y.emplace_back(true);
+                        } else {
+                            Y.emplace_back(false);
+                        }
+                    }
+                }
+                feature_vectors.emplace_back(filaData);
+                numeroFilas++;
+            }
+        }
+        archivo.close();
+    }
+
+    void initialization(){}
+
+    void init_matrix(Mdouble *mat){} //Inicializacion de matrices de pesos con valores random
+
+    void fit(int epochs,double lrate){}
+
+    void forward(vector<double> X_f,int flag) {}
+
+    void backward(int imgid,double lrate,int epoch) {}
+
+    void shuffleAndSplit(float train=1) {}
+
+    void testing(){}
+};
+
+#endif
+
+int main(){
+    boost::container::vector<ActivationFunction*> actfunct;
+    actfunct.push_back(new ActivationFunction());
+    
+    vec initial_states(2, 3);
+    initial_states.pop_back(); 
+
+    MLP mlp(initial_states, 1, actfunct);
     return 0;
 }
