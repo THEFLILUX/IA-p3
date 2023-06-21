b283b34e753759fde58651a1e53398c764769178
mateonoel2 <mateo.noel@utec.edu.pe>
2023-06-20 20:31:37 -0500

    ignore

--- main.cpp
+++ main.cpp
@@ -20,65 +20,25 @@ class ActivationFunction {
         double apply(string activationFunction, double value) {
             if (activationFunction == "sigmoid") {
                 return sigmoid(value);
-            } else if (activationFunction == "relu") {
+            } 
+            else if (activationFunction == "relu") {
                 return relu(value);
-            } else if (activationFunction == "tanh") {
+            }
+            else if (activationFunction == "tanh") {
                 return tanh(value);
-            } else {
+            } 
+            else {
                 return value;
             }
         }
 };
 
-// class Neuron {
-//     private:
-//         double value;
-//         double bias;
-//         double weight;
-//         ActivationFunction activationFunction;
-//     public:
-//         Neuron(double value, double bias, double weight, ActivationFunction activationFunction) {
-//             this->value = value;
-//             this->bias = bias;
-//             this->weight = weight;
-//             this->activationFunction = activationFunction;
-//         }
-// };
-
-// class Layer {
-//     private:
-//         Neuron neurons[];
-//     public:
-//         Layer(Neuron neurons[]) {
-//             this->neurons = neurons;
-//         }
-// };
-
-// class MLP {
-//     private:
-//         Layer layers[];
-//     public:
-//         MLP(Layer layers[]) {
-//             this->layers = layers;
-//         }
-// };
-
 int main() {
     cout << "Hello World!" << endl;
 
-    // Initalize MLP with data
-
-    // Initialize layer and neurons
-
-    // Initialize weights
-
-    // Initialize bias
-
-    // Initialize activation function
-
     ActivationFunction activationFunction;
     double value = activationFunction.apply("sigmoid", 0.5);
-    // Print value double
+
     cout << value << endl;
     return 0;
 }
