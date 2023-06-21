459aaff2dd61049c511ff447be8d24d68393e027
mateonoel2 <mateo.noel@utec.edu.pe>
2023-06-20 23:28:49 -0500

    funciona

--- MLP.h
+++ MLP.h
@@ -1,153 +1,100 @@
-#ifndef MLP_H
-#define MLP_H
+//
+// Created by Lyndon Duong on 12/21/21.
+//
+#include "matrix.h"
+#include <random>
+#include <utility>
+#include <cassert>
 
-#include <boost/container/vector.hpp>
-#include <boost/numeric/ublas/io.hpp>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/operation.hpp>
-#include <boost/random.hpp>
-#include "ActivationFunction.cpp"
-using namespace std;
+using namespace lynalg;
+namespace mlp {
 
+// activation function
+inline float sigmoid(float x) {
+  return 1.0f / (1 + exp(-x));
+}
+
+// sigmoid derivative for backprop
+inline float d_sigmoid(float x){
+  return (x * (1 - x));
+}
+
+template<typename T>
 class MLP {
-    typedef boost::numeric::ublas::matrix<double> Mdouble;
-    typedef boost::container::vector<int> vec;
-private:
+ public:
+  std::vector<size_t> units_per_layer;
+  std::vector<Matrix<T>> bias_vectors;
+  std::vector<Matrix<T>> weight_matrices;
+  std::vector<Matrix<T>> activations;
 
-    boost::container::vector<ActivationFunction*>f; //Neuronas por capa
+  float lr;
 
-    vec pPerLayer; //Numero de perceptrones por capa (hidden layer)
+  explicit MLP(std::vector<size_t> units_per_layer, float lr = .001f) :
+      units_per_layer(units_per_layer),
+      weight_matrices(),
+      bias_vectors(),
+      activations(),
+      lr(lr) {
 
-    boost::container::vector<Mdouble> pesos; //Pesos
+    for (size_t i = 0; i < units_per_layer.size() - 1; ++i) {
+      size_t in_channels{units_per_layer[i]}, out_channels{units_per_layer[i+1]};
+      Matrix<T> W  = lynalg::mtx<T>::randn(out_channels, in_channels);
+      weight_matrices.push_back(W);
 
-    boost::container::vector<Mdouble> outputs; //Outputs antes de la funcion de activacion
+      Matrix<T> b  = lynalg::mtx<T>::randn(out_channels, 1);
+      bias_vectors.push_back(b);
 
-    int n_features,n_outputs,n_capas; // num capas ocultas
-
-    boost::container::vector <boost::container::vector<double>> feature_vectors; //Vectores Caracteristicas
-    boost::container::vector <bool> Y;
-
-    boost::container::vector <boost::container::vector<double>> X_train, X_test;
-    boost::container::vector<bool> Y_train, Y_test;
-
-    Mdouble input;
-    Mdouble MLP_ouput;
-
-public:
-    MLP(vec initial_states,int n_out,boost::container::vector<ActivationFunction*> actfunct) {
-        pPerLayer = initial_states;
-        n_capas = pPerLayer.size();
-        n_outputs = n_out;
-        f = actfunct;
-        load_data();
-        n_features = feature_vectors[0].size();
-
-        initialization();
-        shuffleAndSplit();
+      activations.resize(units_per_layer.size());
     }
+  }
 
-    void load_data(){
-        ifstream archivo("data.csv");
-        if (archivo.is_open()) {
-            string campos[128], fila;
-            int numeroFilas = 0;
-            while (!archivo.eof()) {
-                getline(archivo, fila);
-                istringstream stringStream(fila);
-                unsigned int contador = 0;
-                while (getline(stringStream, fila, ',')) {
-                    campos[contador] = fila;
-                    contador++;
-                }
-                boost::container::vector <double> filaData;
-                for (int i = 0; i < 128; i++) {
-                    if (i != 1) {
-                        filaData.emplace_back(stof(campos[i]));
-                    } else {
-                        if (campos[i] == "M") {
-                            Y.emplace_back(true);
-                        } else {
-                            Y.emplace_back(false);
-                        }
-                    }
-                }
-                feature_vectors.emplace_back(filaData);
-                numeroFilas++;
-            }
-        }
-        archivo.close();
+  Matrix<T> forward(Matrix<T> x) {
+    assert(get<0>(x.shape) == units_per_layer[0] && get<1>(x.shape));
+
+    activations[0] = x;
+    Matrix prev(x);
+    for (int i = 0; i < units_per_layer.size() - 1; ++i) {
+
+      Matrix y = weight_matrices[i].matmul(prev);
+      y = y + bias_vectors[i];
+      y = y.apply_function(mlp::sigmoid);
+      activations[i+1] = y;
+      prev = y;
     }
+    return prev;
+  }
+  Matrix<T> operator()(Matrix<T> x) {
+    return forward(x);
+  }
 
-    void initialization(){
-        Mdouble aux;
-        input = Mdouble(1,n_features);
-        init_matrix(&input);
+  void backprop(Matrix<T> target) {
+    assert(get<0>(target.shape) == units_per_layer.back());
 
-        //output = Mdouble(1,n_outputs);
-        //init_matrix(&output);
+    // determine the simple error
+    // error = target - output
+    auto y = target;
+    auto y_hat = activations.back();
+    auto error = (target - y_hat);
 
-        for(int i = 0;i<n_capas;i++){
-            //Creo matriz de pesos y lo agrego al vector de pesos
-            if(i==0){
-                aux = Mdouble(n_features,pPerLayer[i]);
-                init_matrix(&aux);
-                pesos.push_back(aux);
-            }
-            else {
-                aux = Mdouble(pPerLayer[i-1],pPerLayer[i]);
-                init_matrix(&aux);
-                pesos.push_back(aux);
-            }
-        }
-        aux = Mdouble(pPerLayer[n_capas-1],n_outputs);
-        init_matrix(&aux);
-        pesos.push_back(aux);
+    // backprop the error from output to input and step the weights
+    for(int i = weight_matrices.size() - 1 ; i >= 0; --i) {
+      //calculating errors for previous layer
+      auto Wt = weight_matrices[i].T();
+      auto prev_errors = Wt.matmul(error);
+
+      auto d_outputs = activations[i+1].apply_function(d_sigmoid);
+      auto gradients = error.multiply_elementwise(d_outputs);
+      gradients = gradients.multiply_scalar(lr);
+      auto a_trans = activations[i].T();
+      auto weight_gradients = gradients.matmul(a_trans);
+
+      //adjust weights
+      bias_vectors[i] = bias_vectors[i].add(gradients);
+      weight_matrices[i] = weight_matrices[i].add(weight_gradients);
+      error = prev_errors;
     }
+  }
 
-    void init_matrix(Mdouble *mat){ //Inicializacion de matrices de pesos con valores random
-        std::time_t now = std::time(0);
-        boost::random::mt19937 gen{static_cast<std::uint32_t>(now)};
-        boost::random::uniform_int_distribution<> dist{1, 10000};
-        for (int i = 0;i< mat->size1();i++){
-            for (int j = 0;j< mat->size2();j++)
-                mat->operator()(i,j)= double(dist(gen)/10000.0);
-        }
-    }
-
-    void fit(int epochs,double lrate){
-        for(int i = 0;i<epochs;i++){
-            cout<<"Epoch"<<i+1<<endl;
-            for(int j = 0;j<X_train.size();j++){
-                //cout<<"size"<<X_f.size()<<endl;
-                forward(X_train[j],0);
-                backward(j,lrate,i);
-            }
-        }
-    }
-
-    void forward(boost::container::vector <double> X_f,int flag) {}
-
-    void backward(int imgid,double lrate,int epoch) {}
-
-    void shuffleAndSplit(float train=1) {
-        int ft_size = feature_vectors.size();
-        // Realizar el shuffle de la data
-        srand(10);
-        for (int i=0; i<ft_size; i++) {
-            int j = i + rand() % (ft_size - i);
-            swap(feature_vectors[i], feature_vectors[j]);
-            swap(Y[i], Y[j]);
-        }
-        for (int i=0; i<ft_size; i++) {
-            if (i < ft_size*train) {
-                X_train.push_back(feature_vectors[i]);
-                Y_train.push_back(Y[i]);
-            } else {
-                X_test.push_back(feature_vectors[i]);
-                Y_test.push_back(Y[i]);
-            }
-        }
-    }
 };
 
-#endif
+}
--- main.cpp
+++ main.cpp
@@ -1,44 +1,115 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include "MLP.h"
+#include <fstream>
+#include <deque>
+#include <iomanip>
 
-class ActivationFunction {
-    private:
-        double value;
-    public:
-        double sigmoid(double value) {
-            return 1 / (1 + exp(-value));
-        }
+// helper to initialize multi-layer perceptron with n hidden layers each w/ same num hidden units
+auto make_model(size_t in_channels,
+                          size_t out_channels,
+                          size_t hidden_units_per_layer,
+                          int hidden_layers,
+                          float lr) {
+  std::vector<size_t> units_per_layer;
 
-        double relu(double value) {
-            return max(0.0, value);
-        }
+  units_per_layer.push_back(in_channels);
 
-        double tanh(double value) {
-            return (exp(value) - exp(-value)) / (exp(value) + exp(-value));
-        }
+  for (int i = 0; i < hidden_layers; ++i)
+    units_per_layer.push_back(hidden_units_per_layer);
 
-        double apply(string activationFunction, double value) {
-            if (activationFunction == "sigmoid") {
-                return sigmoid(value);
-            } 
-            else if (activationFunction == "relu") {
-                return relu(value);
-            }
-            else if (activationFunction == "tanh") {
-                return tanh(value);
-            } 
-            else {
-                return value;
-            }
-        }
-};
+  units_per_layer.push_back(out_channels);
+
+  mlp::MLP<float> model(units_per_layer, 0.01f);
+  return model;
+}
+
+auto mean(const auto &d) {
+  float mu {0.};
+  for(auto v: d){
+    mu += v;
+  }
+  return mu/d.size();
+}
+
+void log(auto &file, const auto &x, const auto &y, const auto &y_hat){
+  auto mse = (y.data[0] - y_hat.data[0]);
+  mse = mse*mse;
+
+  file << mse << " "
+       << x.data[0] << " "
+       << y.data[0] << " "
+       << y_hat.data[0] << " \n";
+}
 
 int main() {
-    cout << "Hello World!" << endl;
+    std::srand(21);
 
-    ActivationFunction activationFunction;
-    double value = activationFunction.apply("sigmoid", 0.5);
+    std::ifstream file("train.csv");
+    std::string line;
+    std::getline(file, line);  
+    
+    lynalg::Matrix<float> data(487, 128);
+    lynalg::Matrix<float> labels(487, 1);
 
-    cout << value << endl;
-    return 0;
+    int cont = 0; 
+    int cont2 = 0;
+
+    while (std::getline(file, line) and cont < 487) {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<float> rowData;
+
+        while (std::getline(lineStream, cell, ',')) {
+            rowData.push_back(std::stod(cell));
+        }
+        
+        // Label is the last element of the row
+        labels.data[cont] = rowData.back();
+
+        // Data are all elements except the last one
+        for (int i = 0; i < rowData.size()-1; i++) {
+            data.data[cont2] = rowData[i];
+            cont2++;
+        }
+
+        cont++;
+    }
+
+    data.print_shape();
+    labels.print_shape();
+   
+    int in_channels, out_channels, hidden_units_per_layer, hidden_layers;
+    float lr;
+
+    auto model = make_model(in_channels=128, out_channels=1, hidden_units_per_layer=50, hidden_layers=1, lr=.5f);
+
+    // // train
+    std::ofstream my_file;
+    my_file.open ("train.csv");
+    int print_every{10};
+
+    float mse;
+    auto deque = std::deque<float>(print_every);
+
+    data.get_row(0).print_shape();
+
+    for(int i = 0; i < 487; ++i) {
+        // generate (x, y) training data
+        auto x = data.get_row(i);
+        auto y = labels.get_row(i);
+
+        auto y_hat = model(x.transpose());  // forward pass
+        model.backprop(y); // backward pass
+
+        // compute and print error
+        mse = (y - y_hat).square().data[0];
+        deque.push_back(mse);
+
+        if ((i+1)%print_every==0) {
+            log(my_file, x, y, y_hat);
+            my_file << mse << " " << x.data[0] << " " << y.data[0] << " " << y_hat.data[0] << " \n";
+            std::cout << std::setprecision(4) << std::scientific << "iter: " << i << " -- loss: " << mean(deque) << std::endl;
+        }
+
+    }
+
 }
