05cc1f4cb92b0ac84180f655bf3132d625b31f3d
mateonoel2 <mateo.noel@utec.edu.pe>
2023-06-23 23:03:49 -0500

    a

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@
 std::vector<float> normalizeVector(const std::vector<float>& values) {
     float minVal = *std::min_element(values.begin(), values.end());
     float maxVal = *std::max_element(values.begin(), values.end());
-    std::cout<<minVal<<" "<<maxVal<<std::endl;
-
+    
     std::vector<float> normalizedValues;
     for (const auto& value : values) {
         float normalizedValue = (value - minVal) / (maxVal - minVal);
@@ -60,13 +59,13 @@ int main() {
     std::string line;
     std::getline(file, line);  
     
-    lynalg::Matrix<float> data(487, 128);
-    lynalg::Matrix<float> labels(487, 1);
+    lynalg::Matrix<float> data(1944, 128);
+    lynalg::Matrix<float> labels(1944, 1);
 
     int cont = 0; 
     int cont2 = 0;
 
-    while (std::getline(file, line) and cont < 487) {
+    while (std::getline(file, line) and cont < 1944) {
         std::stringstream lineStream(line);
         std::string cell;
         std::vector<float> rowData;
@@ -87,14 +86,10 @@ int main() {
         cont++;
     }
 
-    data.print_shape();
-    
-    labels.print_shape();
-    
     //normalize data and labels
     labels.data = normalizeVector(labels.data); 
 
-    data = data.add_scalar(819);
+    data = data.add_scalar(1000);
 
     data.data = normalizeVector(data.data);
 
@@ -103,7 +98,7 @@ int main() {
     int in_channels, out_channels, hidden_units_per_layer, hidden_layers;
     float lr;
 
-    auto model = make_model(in_channels=128, out_channels=1, hidden_units_per_layer=100, hidden_layers=1, lr=.5f);
+    auto model = make_model(in_channels=128, out_channels=1, hidden_units_per_layer=200, hidden_layers=1, lr=.5f);
 
     // // train
     
@@ -112,12 +107,12 @@ int main() {
 
     int epochs = 3;
     int min_loss = 1e9;
-
+    
     for(int epoch = 0; epoch < epochs; ++epoch) {
 
         deque.clear();
         
-        for(int i = 0; i < 487; ++i) {
+        for(int i = 0; i < 1944; ++i) {
             // generate (x, y) training data
             auto x = data.get_row(i);
             auto y = labels.get_row(i);
@@ -131,31 +126,77 @@ int main() {
             
         }
     
-        std::cout << std::setprecision(4) << std::scientific << "iter: " << epoch << " -- loss: " << mean(deque) << std::endl;
+        std::cout << std::setprecision(4) << "iter: " << epoch << " -- loss: " << mean(deque) << std::endl;
         
     }
 
-    // print accuracy:
-    float cnt = 0;
-    labels.data = unnormalizeVector(labels.data, 1, 24);
+    std::ifstream file2("test.csv");
+    std::getline(file2, line);  
+    lynalg::Matrix<float> data2(487, 128);
+    lynalg::Matrix<float> labels2(487, 1);
+    cont = 0; 
+    cont2 = 0;
 
-    for (int i = 0; i<labels.data.size(); i++){
-        auto x = data.get_row(i);
-        auto y = labels.get_row(i);
-        auto y_hat = model(x.transpose());
-        y_hat.data = unnormalizeVector(y_hat.data, 1, 24);
+    while (std::getline(file2, line) and cont < 487) {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<float> rowData;
+
+        while (std::getline(lineStream, cell, ',')) {
+            rowData.push_back(std::stod(cell));
+        }
         
-        if (y.data[0] - y_hat.data[0] < 1){
-            cnt++;
+        // Label is the last element of the row
+        labels2.data[cont] = rowData.back();
+
+        // Data are all elements except the last one
+        for (int i = 0; i < rowData.size()-1; i++) {
+            data2.data[cont2] = rowData[i];
+            cont2++;
         }
 
-        std::cout<< "y: " << y.data[0] << "\t||\t y_hat: " << y_hat.data[0] << std::endl;
+        cont++;
+    }
+
+    labels2.data = normalizeVector(labels2.data); 
+    data2 = data2.add_scalar(1000);
+    data2.data = normalizeVector(data2.data);
+
+    float cnt = 0;
+    labels2.data = unnormalizeVector(labels2.data, 1, 24);
+
+    int truePositives = 0;
+    int falsePositives = 0;
+    int falseNegatives = 0;
+
+    for (int i = 0; i < labels2.data.size(); i++) {
+    auto x = data2.get_row(i);
+    auto y = labels2.get_row(i);
+    auto y_hat = model(x.transpose());
+    y_hat.data = unnormalizeVector(y_hat.data, 1, 24);
+
+    if (y.data[0] - y_hat.data[0] < 1) {
+        truePositives++;
+    } else {
+        falseNegatives++;
     }
     
-    float acc = cnt/487.f;
-    std::cout << "Accuracy: " << acc << std::endl;
+    if (y_hat.data[0] < y.data[0] - 1 || y_hat.data[0] > y.data[0] + 1) {
+        falsePositives++;
+    }
+    }
 
+    float accuracy = static_cast<float>(truePositives) / labels2.data.size();
+    std::cout << "Accuracy: " << accuracy << std::endl;
 
+    float precision = static_cast<float>(truePositives) / (truePositives + falsePositives);
+    std::cout << "Precision: " << precision << std::endl;
+
+    float recall = static_cast<float>(truePositives) / (truePositives + falseNegatives);
+    std::cout << "Recall: " << recall << std::endl;
+
+    float f1Score = 2.0 * (precision * recall) / (precision + recall);
+    std::cout << "F1 Score: " << f1Score << std::endl;
 
     return 0;
 }
